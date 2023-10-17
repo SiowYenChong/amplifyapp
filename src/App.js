c809d70b200b0ef0615f4630329bda0fd3712905
SiowYenC <Clairechong998@gmail.com>
2023-10-17 15:36:34 +0800

    name change - app.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from Siow Yen</h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
